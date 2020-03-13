58b6df0088c627b5992ad17e2ffa1b6788de7506
olivercastroteran <olivercastroteran@gmail.com>
2020-03-13 11:32:58 -0400

    Bright controller update

--- Cool-Stuff/Bright-Controller/app.js
+++ Cool-Stuff/Bright-Controller/app.js
@@ -1,6 +1,19 @@
 let rangeInput = document.getElementById('range');
+const lowBright = document.querySelector('.minus');
+const moreBright = document.querySelector('.plus');
 const container = document.getElementsByClassName('container')[0];
 
 rangeInput.addEventListener('change', function() {
+  container.style.filter = 'brightness(' + rangeInput.value + '%)';
+});
+
+lowBright.addEventListener('click', function() {
+  rangeInput.value -= 5;
+  console.log(rangeInput.value);
+  container.style.filter = 'brightness(' + rangeInput.value + '%)';
+});
+
+moreBright.addEventListener('click', function() {
+  rangeInput.value += 5;
   container.style.filter = 'brightness(' + rangeInput.value + '%)';
 });
